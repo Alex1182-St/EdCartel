7e7e03598857cda9ff840316e313f357bb33e3fb
OlekSt <alexander.stepaniuk@yahoo.com.ua>
2020-04-14 15:17:04 +0300

    Merged with test branch

--- src/main/kotlin/edcartel/course/services/CourseService.kt
+++ src/main/kotlin/edcartel/course/services/CourseService.kt
@@ -10,11 +10,21 @@ import java.util.*
 class CourseService (val courseRepo : CourseRepository)
 {
     fun updateCourse(courseId : UUID,  course : CourseEntity) : CourseEntity {
+
         val oldCourse = courseRepo.findById(courseId)
                 .orElseThrow{ Exception("Course not found with such id: $courseId") }
-
-        return if (oldCourse != course) {
-                courseRepo.save(course)
+        val authorsFromOldCourse = oldCourse.author
+        val authorsFromNewCourse = course.author
+/*
+        for (n in authorsFromNewCourse)
+        {
+            for (o in authorsFromOldCourse) {
+                authorsFromNewCourse.
+            }
+        }
+*/
+        return if (oldCourse != course && authorsFromOldCourse == authorsFromNewCourse) {
+                  courseRepo.save(course)
         }
         else {
             throw Exception("There are nothing to update with course - courseId: $courseId")}
